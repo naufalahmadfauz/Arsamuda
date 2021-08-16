802ad60e51d47ef24ceca7d5aab8a0023327025d
naufalahmadfauz <supernaufalboy@gmail.com>
2021-08-16 18:48:09 +0700

    first commit,rebase to Arsamuda

---
+++ src/email/account.js
@@ -0,0 +1,25 @@
+const mailgun = require("mailgun-js")({apiKey:process.env.MAILGUN_API_KEY,domain:'masakasik.me'})
+
+
+const sendWelcomeEmail = (email,name)=>{
+    mailgun.messages().send({
+        to:email,
+        from:'supernaufalboy@gmail.com',
+        subject:'Terimakasih Telah Bergabung Dengan TheBoyzRadio',
+        text:`Selamat datang di TheBoyzRadio, ${name}. Stay tune di TheBoyzRadio!`
+    })
+}
+
+const sendCancelationEmail = (email,name)=>{
+    mailgun.messages().send({
+        to:email,
+        from:'supernaufalboy@gmail.com',
+        subject:'Terimakasih Telah TuneIn di TheBoyzRadio',
+        text:`Selamat tinggal, ${name}. Kami sedih melihat anda meninggalkan aplikasi kami. Tolong beritahu kamu untuk menjadi lebih baik kedepannya. Terimakasih!`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
