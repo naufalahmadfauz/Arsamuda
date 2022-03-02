e676ed873d21bbfeabe7810cc2fa9900a87a6983
naufalahmadfauz <supernaufalboy@gmail.com>
2022-03-02 17:37:35 +0700

    integrated captcha work 90% complete

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router()
 const Mail = require('../models/Mailing')
+const fetch = require('node-fetch')
 
 router.get('/',async (req,res) => {
     res.render('coming_soon')
@@ -8,11 +9,23 @@ router.get('/',async (req,res) => {
 
 router.post('/subscribe',async (req,res)=>{
     const mail = new Mail(req.body)
+    const secrettoken = process.env.SECRET_TOKEN
+    const clienttoken = req.body['g-recaptcha-response']
+    const clientIP = req.ip
     try {
-        await mail.save()
-        console.log(req.body)
-        res.status(200).render('coming_soon',{titlepage:'Sukses'})
-        // res.status(201).send('Saved')
+        const params = new URLSearchParams();
+        params.append('secret', secrettoken);
+        params.append('response',clienttoken)
+        params.append('remoteip',clientIP)
+        const responsecaptcha = await fetch('https://www.google.com/recaptcha/api/siteverify', {method: 'POST', body: params});
+        const dataCaptcha = await responsecaptcha.json();
+        if (dataCaptcha.success !==false) {
+            await mail.save()
+            res.status(200).render('coming_soon',{titlepage:'Sukses'})
+        }else {
+            res.status(200).render('coming_soon',{titlepage:'failed'})
+        }
+        // res.status(201).send(params.toString())
     }catch (e){
         res.status(500).send(e)
     }
@@ -21,6 +34,4 @@ router.post('/subscribe',async (req,res)=>{
 router.get('/live',async (req,res) => {
     res.render('home')
 })
-
-
 module.exports = router
