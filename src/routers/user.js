c8f4875144ab30f1ed27b0d87956d3ee223591e4
naufalahmadfauz <supernaufalboy@gmail.com>
2022-05-27 20:04:30 +0700

    upload download list blobs work individually,integrating with multer now

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,6 +6,17 @@ const multer = require('multer')
 const sharp = require('sharp')
 const randomString = require('randomstring')
 const path = require("path");
+const {BlobServiceClient} = require("@azure/storage-blob");
+const util = require('util')
+const {createReadStream, createWriteStream, pipe} = require('fs')
+//connection string dari storage accout
+// const connStr = "DefaultEndpointsProtocol=https;AccountName=arsamudaazurestorage;AccountKey=QZRUx8cPlhYMJtLhpWZOahP0xKSqCdvYFTPYbArjMIbjmTqwS+rkgUfLGWqdHX6cG0ZNhGqy3R43mcOawU1wXw==;EndpointSuffix=core.windows.net";
+const connStr = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1";
+//inisialisasi atau buka koneksi ke azure dengan connection string
+const blobServiceClient = BlobServiceClient.fromConnectionString(connStr);
+const containerName = "arsamudapicturestorage";
+const containerClient = blobServiceClient.getContainerClient(containerName);
+const blobName = "Capture.PNG";
 
 const upload = multer({
     limits: {
@@ -25,7 +36,7 @@ const uploadCoverStorage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         const extfilename = path.extname(file.originalname)
-        cb(null, file.fieldname + '_' + Date.now() + '_' + randomString.generate({length: 5})+extfilename)
+        cb(null, file.fieldname + '_' + Date.now() + '_' + randomString.generate({length: 5}) + extfilename)
     }
 })
 
@@ -85,6 +96,7 @@ router.post('/users/logout', auth, async (req, res) => {
 //Implement logoutAll
 
 router.get('/users/me', auth, async (req, res) => {
+
     res.send(req.user)
 })
 
@@ -149,8 +161,66 @@ router.post('/users/me/cover', auth, uploadCover.single('cover'), async (req, re
     } catch (e) {
         res.status(500).send(e)
     }
-},(error,req,res,next)=>{
+}, (error, req, res, next) => {
     res.status(400).send({error: error.message})
 })
+
+
+router.get('/createazurecontainer', async (req, res, next) => {
+    //dapatkan nama container dari containername dengna koneksi yang sudah dibuka dari blobserviceclient
+
+    //upload query buat bikin container nya ke azure,fungsi asyncronous karena butuh waktu
+    const createContainerResponse = await containerClient.create();
+    console.log(`Create container ${containerName} successfully`, createContainerResponse.requestId)
+
+    res.send()
+})
+
+router.get('/listazurecontainer', async (req, res, next) => {
+    let i = 1;
+    let containers = blobServiceClient.listContainers();
+    for await (const container of containers) {
+        console.log(`Container ${i++}: ${container.name}`);
+    }
+    res.send()
+})
+
+router.get('/uploadblob', async (req, res, next) => {
+    try {
+
+        const imagepath = path.join(__dirname, '../../public/img/meme.jpg')
+        const pic = createReadStream(imagepath, {highWaterMark: 8000000})
+
+        const blockBlobClient = containerClient.getBlockBlobClient(req.body.pictureName);
+        const uploadBlobResponse = await blockBlobClient.uploadStream(pic);
+        console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
+
+        res.status(201).send('OK')
+    } catch (err) {
+        res.status(400).send('NOT OK')
+    }
+})
+
+router.get('/listblobs', async (req, res, next) => {
+    let i = 1;
+    let blobs = containerClient.listBlobsFlat();
+    for await (const blob of blobs) {
+        console.log(`Blob ${i++}: ${blob.name}`);
+    }
+    res.send()
+})
+
+router.get('/downloadblob', async (req, res, next) => {
+    try {
+        const blobClient = containerClient.getBlobClient(req.body.pictureName);
+        const downloadBlockBlobResponse = await blobClient.download(0)
+        console.log("Downloaded blob content", downloadBlockBlobResponse.readableStreamBody);
+        downloadBlockBlobResponse.readableStreamBody.pipe(res)
+    } catch (e) {
+        res.status(404).send('Not Found')
+    }
+
+})
+
 
 module.exports = router
